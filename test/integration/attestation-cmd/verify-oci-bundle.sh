7e69c0d4ab296b5b2dba48f01cd8a9f1483853f7
Meredith Lancaster <malancas@github.com>
2024-12-05 10:06:26 -0700

    fix package reference
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- test/integration/attestation-cmd/verify-oci-bundle.sh
+++ test/integration/attestation-cmd/verify-oci-bundle.sh
@@ -7,7 +7,7 @@ rootDir="$(git rev-parse --show-toplevel)"
 ghBuildPath="$rootDir/bin/gh"
 
 # Verify an OCI artifact with bundles stored on the GHCR OCI registry
-echo "Testing with package $sigstore02PackageFile and attestation $sigstore02AttestationFile"
+echo "Testing with OCI image ghcr.io/malancas/attest-demo:latest with the --bundle-from-oci flag"
 if ! $ghBuildPath attestation verify oci://ghcr.io/malancas/attest-demo:latest --owner=malancas --bundle-from-oci; then
     echo "Failed to verify oci://ghcr.io/malancas/attest-demo:latest with bundles from the GHCR OCI registry"
     exit 1
